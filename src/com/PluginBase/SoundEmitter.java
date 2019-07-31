9774a6c52b55e226ae1dfebbac592408d7097541
Arian Dannemann <arian.dannemann@gmail.com>
2019-07-31 13:06:59 +0200

    Added JavaDoc description to sound emitter

--- src/com/PluginBase/SoundEmitter.java
+++ src/com/PluginBase/SoundEmitter.java
@@ -21,6 +21,20 @@ public class SoundEmitter {
 		return soundEmitter;
 	}
 	
+	/**
+	 * Will randomly output sounds at an entities location
+	 * 
+	 * @param entity			The entity where the sounds should be emitted
+	 * @param sound				The sound that should be emitted
+	 * @param soundCategory		The category of the sound that should be emitted
+	 * @param volume			The sounds volume
+	 * @param pitch				The sounds pitch
+	 * @param chance			The chance of a sound being emitted every period
+	 * @param plugin			The plugin
+	 * @param delay				The delay before the first sound has a chance to be emitted
+	 * @param period			The period at which a sound will be emitted at a chance
+	 * @param duration			The duration for which random sound will be emitted
+	 */
 	public void emitContinuousSoundsRandomly(Entity entity, Sound sound, SoundCategory soundCategory, float volume, float pitch, int chance, Plugin plugin, int delay, int period, int duration) {
 		
 		// Create a synchronous task
