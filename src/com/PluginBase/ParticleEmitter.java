895fed66cc382d2afdae4c1cba5309f05b4cc54f
Arian Dannemann <arian.dannemann@gmail.com>
2020-05-07 12:41:33 +0200

    Improved some formatting

--- src/com/PluginBase/ParticleEmitter.java
+++ src/com/PluginBase/ParticleEmitter.java
@@ -18,63 +18,64 @@ import org.bukkit.util.Vector;
 public class ParticleEmitter {
 
 	public static final ParticleEmitter particleEmitter = new ParticleEmitter();
-	
+
 	public static ParticleEmitter getInstance() {
 		return particleEmitter;
 	}
-	
-	public Particle[] particleTypesThatNeedData = new Particle[] {
-			Particle.REDSTONE,
-			Particle.ITEM_CRACK,
-			Particle.BLOCK_CRACK,
-			Particle.BLOCK_DUST
-	};
-	
+
+	public Particle[] particleTypesThatNeedData = new Particle[] { Particle.REDSTONE, Particle.ITEM_CRACK,
+			Particle.BLOCK_CRACK, Particle.BLOCK_DUST };
+
 	/**
 	 * Emit particles for a certain amount of time at the location of an entity
 	 * 
-	 * @param entity 		The entity at which the particles will be emitted
-	 * @param particle 		The particle type to be emitted
-	 * @param amount 		How many particles should be emitted
-	 * @param speed			How quickly the particles should play their animation
-	 * @param spread 		The spread of the particles (using the entity location as a base)
-	 * @param plugin 		The plugin
-	 * @param delay 		Delay in minecraft ticks until the first particle emits (20 ticks = 1 second)
-	 * @param period 		Delay in minecraft ticks between particle emissions (20 ticks = 1 second)
-	 * @param duration 		Duration in minecraft ticks of how long particles will be emitted (20 ticks = 1 second)
+	 * @param entity
+	 *            The entity at which the particles will be emitted
+	 * @param particle
+	 *            The particle type to be emitted
+	 * @param amount
+	 *            How many particles should be emitted
+	 * @param speed
+	 *            How quickly the particles should play their animation
+	 * @param spread
+	 *            The spread of the particles (using the entity location as a base)
+	 * @param plugin
+	 *            The plugin
+	 * @param delay
+	 *            Delay in minecraft ticks until the first particle emits (20 ticks
+	 *            = 1 second)
+	 * @param period
+	 *            Delay in minecraft ticks between particle emissions (20 ticks = 1
+	 *            second)
+	 * @param duration
+	 *            Duration in minecraft ticks of how long particles will be emitted
+	 *            (20 ticks = 1 second)
 	 */
-	public void emitParticlesContinuously(Entity entity, Particle particle, int amount, double speed, Vector spread, Plugin plugin, int delay, int period, int duration) {
-		
+	public void emitParticlesContinuously(Entity entity, Particle particle, int amount, double speed, Vector spread,
+			Plugin plugin, int delay, int period, int duration) {
+
 		// Create a synchronous task
 		BukkitRunnable runnable = new BukkitRunnable() {
-			
+
 			// Keep track of how many times runnable was run
 			int counter = 0;
-			
+
 			@Override
 			public void run() {
-				
+
 				// Emit particles
-				emitParticles
-				(
-						entity.getWorld(),
-						entity.getLocation(),
-						particle,
-						amount,
-						speed,
-						spread
-				);
-				
+				emitParticles(entity.getWorld(), entity.getLocation(), particle, amount, speed, spread);
+
 				// Add the time between runs to timer count
 				this.counter += period;
-				
+
 				// If counter has run for the needed amount of time
 				if (this.counter >= duration) {
-					
+
 					// Cancel this runnable
 					this.cancel();
 				}
-				
+
 				if (entity.isDead()) {
 					this.cancel();
 				}
@@ -82,58 +83,61 @@ public class ParticleEmitter {
 		};
 		runnable.runTaskTimer(plugin, delay, period);
 	}
-	
-	
+
 	/**
 	 * Emit particles for a certain amount of time at the location of an entity
 	 * 
-	 * @param entity 		The entity at which the particles will be emitted
-	 * @param offset 		The offset from the entity where the particles will be emitted
-	 * @param particle 		The particle type to be emitted
-	 * @param amount 		How many particles should be emitted
-	 * @param speed			How quickly the particles should play their animation
-	 * @param spread 		The spread of the particles (using the entity location as a base)
-	 * @param plugin 		The plugin
-	 * @param delay 		Delay in minecraft ticks until the first particle emits (20 ticks = 1 second)
-	 * @param period 		Delay in minecraft ticks between particle emissions (20 ticks = 1 second)
-	 * @param duration 		Duration in minecraft ticks of how long particles will be emitted (20 ticks = 1 second)
+	 * @param entity
+	 *            The entity at which the particles will be emitted
+	 * @param offset
+	 *            The offset from the entity where the particles will be emitted
+	 * @param particle
+	 *            The particle type to be emitted
+	 * @param amount
+	 *            How many particles should be emitted
+	 * @param speed
+	 *            How quickly the particles should play their animation
+	 * @param spread
+	 *            The spread of the particles (using the entity location as a base)
+	 * @param plugin
+	 *            The plugin
+	 * @param delay
+	 *            Delay in minecraft ticks until the first particle emits (20 ticks
+	 *            = 1 second)
+	 * @param period
+	 *            Delay in minecraft ticks between particle emissions (20 ticks = 1
+	 *            second)
+	 * @param duration
+	 *            Duration in minecraft ticks of how long particles will be emitted
+	 *            (20 ticks = 1 second)
 	 */
-	public void emitParticlesContinuously(Entity entity, Vector offset, Particle particle, int amount, double speed, Vector spread, Plugin plugin, int delay, int period, int duration) {
-		
+	public void emitParticlesContinuously(Entity entity, Vector offset, Particle particle, int amount, double speed,
+			Vector spread, Plugin plugin, int delay, int period, int duration) {
+
 		// Create a synchronous task
 		BukkitRunnable runnable = new BukkitRunnable() {
-			
+
 			// Keep track of how many times runnable was run
 			int counter = 0;
-			
+
 			@Override
 			public void run() {
-				
+
 				// Emit particles
-				emitParticles
-				(
-						entity.getWorld(),
-						LocationHelper.getInstance().offsetLocation
-						(
-								entity.getLocation(),
-								offset
-						),
-						particle,
-						amount,
-						speed,
-						spread
-				);
-				
+				emitParticles(entity.getWorld(),
+						LocationHelper.getInstance().offsetLocation(entity.getLocation(), offset), particle, amount,
+						speed, spread);
+
 				// Add the time between runs to timer count
 				this.counter += period;
-				
+
 				// If counter has run for the needed amount of time
 				if (this.counter >= duration) {
-					
+
 					// Cancel this runnable
 					this.cancel();
 				}
-				
+
 				if (entity.isDead()) {
 					this.cancel();
 				}
@@ -141,48 +145,53 @@ public class ParticleEmitter {
 		};
 		runnable.runTaskTimer(plugin, delay, period);
 	}
-	
+
 	/**
 	 * Emit particles for a certain amount of time at a specific location
 	 * 
-	 * @param location 		The location at which the particles will be emitted
-	 * @param particle 		The particle type to be emitted
-	 * @param amount 		How many particles should be emitted
-	 * @param speed 		How quickly the particles should play their animation
-	 * @param spread 		The spread of the particles (using the entity location as a base)
-	 * @param plugin 		The plugin
-	 * @param delay 		Delay in minecraft ticks until the first particle emits (20 ticks = 1 second)
-	 * @param period 		Delay in minecraft ticks between particle emissions (20 ticks = 1 second)
-	 * @param duration 		Duration in minecraft ticks of how long particles will be emitted (20 ticks = 1 second)
+	 * @param location
+	 *            The location at which the particles will be emitted
+	 * @param particle
+	 *            The particle type to be emitted
+	 * @param amount
+	 *            How many particles should be emitted
+	 * @param speed
+	 *            How quickly the particles should play their animation
+	 * @param spread
+	 *            The spread of the particles (using the entity location as a base)
+	 * @param plugin
+	 *            The plugin
+	 * @param delay
+	 *            Delay in minecraft ticks until the first particle emits (20 ticks
+	 *            = 1 second)
+	 * @param period
+	 *            Delay in minecraft ticks between particle emissions (20 ticks = 1
+	 *            second)
+	 * @param duration
+	 *            Duration in minecraft ticks of how long particles will be emitted
+	 *            (20 ticks = 1 second)
 	 */
-	public void emitParticlesContinuously(Location location, Particle particle, int amount, double speed, Vector spread, Plugin plugin, int delay, int period, int duration) {
-		
+	public void emitParticlesContinuously(Location location, Particle particle, int amount, double speed, Vector spread,
+			Plugin plugin, int delay, int period, int duration) {
+
 		// Create a synchronous task
 		BukkitRunnable runnable = new BukkitRunnable() {
-			
+
 			// Keep track of how many times runnable was run
 			int counter = 0;
-			
+
 			@Override
 			public void run() {
-				
+
 				// Emit particles
-				emitParticles
-				(
-						location.getWorld(),
-						location,
-						particle,
-						amount,
-						speed,
-						spread
-				);
-				
+				emitParticles(location.getWorld(), location, particle, amount, speed, spread);
+
 				// Add the time between runs to timer count
 				this.counter += period;
-				
+
 				// If counter has run for the needed amount of time
 				if (this.counter >= duration) {
-					
+
 					// Cancel this runnable
 					this.cancel();
 				}
@@ -190,103 +199,116 @@ public class ParticleEmitter {
 		};
 		runnable.runTaskTimer(plugin, delay, period);
 	}
-	
+
 	/**
 	 * Emit particles once at a specific location
 	 * 
-	 * @param world 		The world in which the particles will be emitted
-	 * @param location 		The location at which the particles will be emitted
-	 * @param particle 		The particle type to be emitted
-	 * @param amount 		How many particles should be emitted
-	 * @param speed 		How quickly the particles should play their animation
-	 * @param spread 		The spread of the particles (using the entity location as a base)
+	 * @param world
+	 *            The world in which the particles will be emitted
+	 * @param location
+	 *            The location at which the particles will be emitted
+	 * @param particle
+	 *            The particle type to be emitted
+	 * @param amount
+	 *            How many particles should be emitted
+	 * @param speed
+	 *            How quickly the particles should play their animation
+	 * @param spread
+	 *            The spread of the particles (using the entity location as a base)
 	 */
-	public void emitParticles(World world, Location location, Particle particle, int amount, double speed, Vector spread) {
-		
+	public void emitParticles(World world, Location location, Particle particle, int amount, double speed,
+			Vector spread) {
+
 		// Loop through all particle types that minecraft expects some data for
 		for (Particle particleThatNeedsData : this.particleTypesThatNeedData) {
-			
+
 			// Check if the particle type given needs data
 			if (particle == particleThatNeedsData) {
-				
+
 				// Log the error in the console
-				Chat.getInstance().sendErrorToConsole
-				(
-						"PluginBase",
-						"Tried to emit particle without data",
-						"Particle." + particle + " may need either dust options or block data to be spawned"
-				);
-				
+				Chat.getInstance().sendErrorToConsole("PluginBase", "Tried to emit particle without data",
+						"Particle." + particle + " may need either dust options or block data to be spawned");
+
 				// Do not actually spawn the particle
 				return;
 			}
 		}
-		
-		world.spawnParticle
-		(
-				particle,					// Particle type
-				location.getX(),			// Location X
-				location.getY(),			// Location Y
-				location.getZ(),			// Location Z
-				amount,						// Particle amount
-				spread.getX(),				// Particle spread X
-				spread.getY(),				// Particle spread Y
-				spread.getZ(),				// Particle spread Z
-				speed						// Particle speed
+
+		world.spawnParticle(particle, // Particle type
+				location.getX(), // Location X
+				location.getY(), // Location Y
+				location.getZ(), // Location Z
+				amount, // Particle amount
+				spread.getX(), // Particle spread X
+				spread.getY(), // Particle spread Y
+				spread.getZ(), // Particle spread Z
+				speed // Particle speed
 		);
 	}
-	
+
 	/**
 	 * Emit redstone dust once at a specific location
 	 * 
-	 * @param world 		The world in which the particles will be emitted
-	 * @param location 		The location at which the particles will be emitted
-	 * @param particle 		The particle type to be emitted
-	 * @param amount 		How many particles should be emitted
-	 * @param speed 		How quickly the particles should play their animation
-	 * @param spread 		The spread of the particles (using the entity location as a base)
-	 * @param dustOptions	The type of the particle dust
+	 * @param world
+	 *            The world in which the particles will be emitted
+	 * @param location
+	 *            The location at which the particles will be emitted
+	 * @param particle
+	 *            The particle type to be emitted
+	 * @param amount
+	 *            How many particles should be emitted
+	 * @param speed
+	 *            How quickly the particles should play their animation
+	 * @param spread
+	 *            The spread of the particles (using the entity location as a base)
+	 * @param dustOptions
+	 *            The type of the particle dust
 	 */
-	public void emitParticles(World world, Location location, Particle particle, DustOptions dustOptions, int amount, double speed, Vector spread) {
-		world.spawnParticle
-		(
-				particle,					// Particle type
-				location.getX(),			// Location X
-				location.getY(),			// Location Y
-				location.getZ(),			// Location Z
-				amount,						// Particle amount
-				spread.getX(),				// Particle spread X
-				spread.getY(),				// Particle spread Y
-				spread.getZ(),				// Particle spread Z
-				speed,						// Particle speed
-				dustOptions					// Particle dust options
+	public void emitParticles(World world, Location location, Particle particle, DustOptions dustOptions, int amount,
+			double speed, Vector spread) {
+		world.spawnParticle(particle, // Particle type
+				location.getX(), // Location X
+				location.getY(), // Location Y
+				location.getZ(), // Location Z
+				amount, // Particle amount
+				spread.getX(), // Particle spread X
+				spread.getY(), // Particle spread Y
+				spread.getZ(), // Particle spread Z
+				speed, // Particle speed
+				dustOptions // Particle dust options
 		);
 	}
-	
+
 	/**
 	 * Emit particles with custom block data once at a specific location
 	 * 
-	 * @param world 		The world in which the particles will be emitted
-	 * @param location 		The location at which the particles will be emitted
-	 * @param particle 		The particle type to be emitted
-	 * @param amount 		How many particles should be emitted
-	 * @param speed 		How quickly the particles should play their animation
-	 * @param spread 		The spread of the particles (using the entity location as a base)
-	 * @param blockData		The type of block the particles should belong to
+	 * @param world
+	 *            The world in which the particles will be emitted
+	 * @param location
+	 *            The location at which the particles will be emitted
+	 * @param particle
+	 *            The particle type to be emitted
+	 * @param amount
+	 *            How many particles should be emitted
+	 * @param speed
+	 *            How quickly the particles should play their animation
+	 * @param spread
+	 *            The spread of the particles (using the entity location as a base)
+	 * @param blockData
+	 *            The type of block the particles should belong to
 	 */
-	public void emitParticles(World world, Location location, Particle particle, BlockData blockData, int amount, double speed, Vector spread) {
-		world.spawnParticle
-		(
-				particle,					// Particle type
-				location.getX(),			// Location X
-				location.getY(),			// Location Y
-				location.getZ(),			// Location Z
-				amount,						// Particle amount
-				spread.getX(),				// Particle spread X
-				spread.getY(),				// Particle spread Y
-				spread.getZ(),				// Particle spread Z
-				speed,						// Particle speed
-				blockData					// Particle block data
+	public void emitParticles(World world, Location location, Particle particle, BlockData blockData, int amount,
+			double speed, Vector spread) {
+		world.spawnParticle(particle, // Particle type
+				location.getX(), // Location X
+				location.getY(), // Location Y
+				location.getZ(), // Location Z
+				amount, // Particle amount
+				spread.getX(), // Particle spread X
+				spread.getY(), // Particle spread Y
+				spread.getZ(), // Particle spread Z
+				speed, // Particle speed
+				blockData // Particle block data
 		);
 	}
 }
--- src/com/PluginBase/SoundEmitter.java
+++ src/com/PluginBase/SoundEmitter.java
@@ -14,59 +14,68 @@ import org.bukkit.scheduler.BukkitRunnable;
  * @author Arian Dannemann
  */
 public class SoundEmitter {
-	
+
 	public static final SoundEmitter soundEmitter = new SoundEmitter();
-	
+
 	public static SoundEmitter getInstance() {
 		return soundEmitter;
 	}
-	
+
 	/**
 	 * Will randomly output sounds at an entities location
 	 * 
-	 * @param entity			The entity where the sounds should be emitted
-	 * @param sound				The sound that should be emitted
-	 * @param soundCategory		The category of the sound that should be emitted
-	 * @param volume			The sounds volume
-	 * @param pitch				The sounds pitch
-	 * @param chance			The chance of a sound being emitted every period
-	 * @param plugin			The plugin
-	 * @param delay				The delay before the first sound has a chance to be emitted
-	 * @param period			The period at which a sound will be emitted at a chance
-	 * @param duration			The duration for which random sound will be emitted
+	 * @param entity
+	 *            The entity where the sounds should be emitted
+	 * @param sound
+	 *            The sound that should be emitted
+	 * @param soundCategory
+	 *            The category of the sound that should be emitted
+	 * @param volume
+	 *            The sounds volume
+	 * @param pitch
+	 *            The sounds pitch
+	 * @param chance
+	 *            The chance of a sound being emitted every period
+	 * @param plugin
+	 *            The plugin
+	 * @param delay
+	 *            The delay before the first sound has a chance to be emitted
+	 * @param period
+	 *            The period at which a sound will be emitted at a chance
+	 * @param duration
+	 *            The duration for which random sound will be emitted
 	 */
-	public void emitContinuousSoundsRandomly(Entity entity, Sound sound, SoundCategory soundCategory, float volume, float pitch, int chance, Plugin plugin, int delay, int period, int duration) {
-		
+	public void emitContinuousSoundsRandomly(Entity entity, Sound sound, SoundCategory soundCategory, float volume,
+			float pitch, int chance, Plugin plugin, int delay, int period, int duration) {
+
 		// Create a synchronous task
 		BukkitRunnable runnable = new BukkitRunnable() {
-			
+
 			// Keep track of how many times runnable was run
 			int counter = 0;
-			
+
 			@Override
 			public void run() {
-				
+
 				// Check if the chance has hit
 				if (MathHelper.getInstance().hasChanceHit(chance)) {
-					
+
 					// Emit particles
-					emitSound
-					(
-							entity.getWorld(),		// world
-							entity.getLocation(),	// location
-							sound,					// sound
-							soundCategory,			// sound category
-							volume,					// volume
-							pitch					// pitch
+					emitSound(entity.getWorld(), // world
+							entity.getLocation(), // location
+							sound, // sound
+							soundCategory, // sound category
+							volume, // volume
+							pitch // pitch
 					);
 				}
-				
+
 				// Add the time between runs to timer count
 				this.counter += period;
-				
+
 				// If counter has run for the needed amount of time
 				if (this.counter >= duration) {
-					
+
 					// Cancel this runnable
 					this.cancel();
 				}
@@ -74,24 +83,30 @@ public class SoundEmitter {
 		};
 		runnable.runTaskTimer(plugin, delay, period);
 	}
-	
+
 	/**
 	 * Emits a sound at a specific location
 	 * 
-	 * @param world				The world in which the sound will be played
-	 * @param location			The location of the world
-	 * @param sound				The sound type
-	 * @param soundCategory		The sound category
-	 * @param volume			The sound volume
-	 * @param pitch				The sound pitch
+	 * @param world
+	 *            The world in which the sound will be played
+	 * @param location
+	 *            The location of the world
+	 * @param sound
+	 *            The sound type
+	 * @param soundCategory
+	 *            The sound category
+	 * @param volume
+	 *            The sound volume
+	 * @param pitch
+	 *            The sound pitch
 	 */
-	public void emitSound(World world, Location location, Sound sound, SoundCategory soundCategory, float volume, float pitch) {
-		world.playSound(
-				location,			// sound location
-				sound,				// sound
-				soundCategory,		// sound category
-				volume,				// volume
-				pitch				// pitch
+	public void emitSound(World world, Location location, Sound sound, SoundCategory soundCategory, float volume,
+			float pitch) {
+		world.playSound(location, // sound location
+				sound, // sound
+				soundCategory, // sound category
+				volume, // volume
+				pitch // pitch
 		);
 	}
 }
